e1f29bfcd73c7c730d18fa51e0ac826b61f5a0ac
wernerdc <manuel.werner3@gmx.de>
2024-07-19 11:00:45 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleFibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConsoleFibunacci \n");
+
+            int x = 0;
+            int y = 1;
+            int newResult = 0;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.Write(x);
+                        break;
+                    case 1:
+                        Console.Write(", " + y);
+                        break;
+                    default:
+                        newResult = x + y;
+                        Console.Write(", " + newResult);
+
+                        x = y;
+                        y = newResult;
+                        break;
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
