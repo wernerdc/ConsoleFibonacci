535539f62cdce470b5b01d404133c46ec4867d5f
wernerdc <manuel.werner3@gmx.de>
2024-07-30 15:03:33 +0200

    comments added

--- ConsoleFibonacci/Program.cs
+++ ConsoleFibonacci/Program.cs
@@ -10,9 +10,14 @@
                 Console.Clear();
                 Console.WriteLine("ConsoleFibunacci \n");
 
+                // get input from console + convert to int. handle any exeptions.
                 int count = GetFibonacciCountFromInput();
+
+                // calculate the fibonacci numbers and output them to console window. Also do not split
+                // any number at the right window border.
                 ShowFibonacciNumbers(count);
 
+                // exit or repeat the app
                 Console.Write("\n\nProgramm beenden (e)? ");
                 try {
                     string exitApp = Console.ReadLine();
@@ -26,15 +31,18 @@
         }
 
         private static int GetFibonacciCountFromInput() {
-            Console.Write("Wieviele Fibonacci-Zahlen sollen berechnet werden? ");
             int count = 0;
             while (count < 1) {
                 try {
+                    // try to convert string from console to integer 
+                    Console.Write("Wieviele Fibonacci-Zahlen sollen berechnet werden? ");
                     count = Convert.ToInt32(Console.ReadLine());
                     if (count <= 0) {
                         ShowErrorInputMessage();
                     }
                 } catch {
+                    // catch errors so the app doesn't crash if one ocours
+                    // then show an error message
                     ShowErrorInputMessage();
                 }
             }
@@ -46,7 +54,6 @@
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("\nUngültige Eingabe: Nur positive Ganzzahlen > 0 sind erlaubt\n");
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("Wieviele Fibonacci-Zahlen sollen berechnet werden? ");
         }
 
         private static void ShowFibonacciNumbers(int count) {
@@ -56,7 +63,7 @@
 
 
             for (int i = 0; i < count; i++) {
-                // exceptions for the first two numbers which are preset in x and y, then begin calculations (x + y)
+                // exceptions for the first two numbers which are allready preset in x and y, then begin calculations (x + y)
                 switch (i) {
                     case 0:
                         Console.Write("\n" + x + ", ");
@@ -68,10 +75,10 @@
                         newResult = x + y; 
 
                         // insert a line break if window width would be reached by next number
+                        //            cursor y position      + formatted newResult lengt + (", ")  >  console window width in chars
                         if (Console.GetCursorPosition().Left + newResult.ToString("N0").Length + 2 >= Console.WindowWidth) {
                             Console.Write("\n");
                         }
-                        
                         Console.Write(newResult.ToString("N0") + ", ");
 
                         x = y;
